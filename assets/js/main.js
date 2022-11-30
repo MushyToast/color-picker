ab858a83f5f6c92ade78546344a3364716f89adc
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-11-30 09:54:43 -0800

    kaka

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-z
+$('document').ready(function(){
+    setInterval(function(){
+    var red = document.getElementsByClassName('red').value
+    var green = document.getElementsByClassName('green').value
+    var blue = document.getElementsByClassName('blue').value
+    const bg = document.getElementById('boody')
+    const dred = 0
+    const dgreen = 0
+    const dblue = 0
+        if (red == 0) {
+            red = 0
+        } else if (green == 0) {
+            green = 0
+        } else if (blue == 0) {
+            blue = 0
+        }
+        bg.style.backgroundColor = rgb(red, green, blue)
+    },
+    2)
+
+})

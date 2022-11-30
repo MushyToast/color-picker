b47b557632fc9657fe4e72adfd20b01148aabe30
galactic_ <ndg72609@gmail.com>
2022-11-29 20:01:39 -0800

    first steps

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,21 @@
+$('document').ready(function(){
+    console.log('Document loaded')
+    var header = document.getElementById('header')
+    var img = document.getElementById('logo')
+    const htext = 'בוחר הצבעים'
+    const etext = 'Color Picker'
+    setInterval(function setHText() {
+        if (header.innerHTML == htext) {
+            header.innerHTML = etext
+            img.src = 'assets/img/DrPicLogo.png'
+            header.id = 'header'
+        } else if (header.innerHTML == etext) {
+            header.innerHTML = htext
+            img.src = 'assets/img/hebrewlogo.png'
+            header.id = 'header2'
 
+        }
+    }, 2000);
+
+
+})
